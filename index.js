34444e71ce91f938ddcd41f2013f8ea550f464c7
Alexander Kosianchuk <dedanaan00@gmail.com>
2017-12-28 01:33:46 +0200

    dashbord enter

--- index.js
+++ index.js
@@ -2,21 +2,20 @@
 var request = require('request');
 var fs = require('fs');
 
-const email = '';
-const pass = '';
+const access = require('./pass');
 
 var options = {
     host: 'http://www.belgameubelen.luch15.com',
-    path: '/apanel/index.php?controller=AdminLogin&token=a9810e5a4510cbca016d79c8d33e1672',
+    path: '/apanel/index.php?controller=AdminLogin&token=',
 };
 
 var request = request.defaults({jar: true})
 
-let authPromise = new Promise((resolve, reject) => {
-    request.post({ url: options.host + options.path,
+let promise = new Promise((resolve, reject) => {
+    request.post({ url: options.host + options.path + access.token,
         form: {
-            email: email,
-            passwd: pass,
+            email: access.email,
+            passwd: access.pass,
             stay_logged_in: 1,
             submitLogin: 1
         }},
@@ -24,6 +23,8 @@ let authPromise = new Promise((resolve, reject) => {
             if (err) {
                 reject(err);
             }
+
+            fs.writeFile('pages/authPromise.html', body);
 
             resolve({
                 cookie: httpResponse.headers['set-cookie'][0],
@@ -33,11 +34,36 @@ let authPromise = new Promise((resolve, reject) => {
     );
 });
 
-authPromise.then((res) => {
-    request.cookie(res.cookie);
-    request.get(options.host + '/apanel/' + res.location,
-        (err, httpResponse, body) => {
-            fs.appendFile('page.html', body);
-        }
-    );
+promise.then((res) => {
+    return new Promise((resolve, reject) => {
+        request.cookie(res.cookie);
+        console.log(res.location);
+        request.get(options.host + '/apanel/' + res.location,
+            (err, httpResponse, body) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    fs.writeFile('pages/dashbordPromise.html', body);
+                    resolve(body);
+                }
+            }
+        );
+    });
+})
+.then((res) => {
+    return new Promise((resolve, reject) => {
+        console.log(options.host + '/apanel/index.php?controller=AdminProducts&token='
+            + access.token + '&setShopContext=s-7');
+        request.get(options.host + '/apanel/index.php?controller=AdminProducts&token='
+            + access.token + '&setShopContext=s-7',
+            (err, httpResponse, body) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    fs.writeFile('pages/productsPromise.html', body);
+                    resolve(body);
+                }
+            }
+        );
+    });
 });
