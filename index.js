6fea09522940b5ad660cb0c3f17d46498401c84e
Alexander Kosianchuk <dedanaan00@gmail.com>
2017-12-27 02:03:33 +0200

    presta update

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
-var http = require('http');
+
+var request = require('request');
 var fs = require('fs');
 
+const email = '';
+const pass = '';
+
 var options = {
-    host: 'www.belgameubelen.luch15.com',
+    host: 'http://www.belgameubelen.luch15.com',
     path: '/apanel/index.php?controller=AdminLogin&token=a9810e5a4510cbca016d79c8d33e1672',
-    method: 'POST'
 };
 
-callback = function(response) {
-  var str = '';
+var request = request.defaults({jar: true})
 
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-      fs.appendFile("page.html", chunk, function(err) {
-        if (err) {
-            return console.log(err);
+let authPromise = new Promise((resolve, reject) => {
+    request.post({ url: options.host + options.path,
+        form: {
+            email: email,
+            passwd: pass,
+            stay_logged_in: 1,
+            submitLogin: 1
+        }},
+        (err, httpResponse, body) => {
+            if (err) {
+                reject(err);
+            }
+
+            resolve({
+                cookie: httpResponse.headers['set-cookie'][0],
+                location: httpResponse.headers.location
+            });
         }
+    );
+});
 
-        console.log("The file was saved!");
-    });
-  });
-
-  //the whole response has been recieved, so we just print it out here
-  response.on('end', function () {
-      //console.log(str);
-  });
-}
-
-var req = http.request(options, callback)
-
-req.write('data\n');
-
-req.on('error', function (error) {
-    console.log(error);
-})
-
-req.end();
+authPromise.then((res) => {
+    request.cookie(res.cookie);
+    request.get(options.host + '/apanel/' + res.location,
+        (err, httpResponse, body) => {
+            fs.appendFile('page.html', body);
+        }
+    );
+});
