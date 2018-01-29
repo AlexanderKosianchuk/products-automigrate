8a8429f555a29a03c888667d1ca0a939679c4dca
Alexander Kosianchuk <dedanaan00@gmail.com>
2018-01-30 01:52:30 +0200

    all products add

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const tools = require('./tools');
 
 var request = request.defaults({ jar: true })
 
+fs.writeFile('js_log.txt', '', () => {});
+
 let promise = new Promise((resolve, reject) => {
     request.post({ url: access.host+access.path+access.token,
         form: {
@@ -20,10 +22,11 @@ let promise = new Promise((resolve, reject) => {
         }},
         (err, httpResponse, body) => {
             if (err) {
+                fs.appendFileSync('js_log.txt', 'auth reject: '+JSON.stringify(err)+'\r\n');
                 reject(err);
             }
 
-            fs.writeFile('pages/authPromise.html', body);
+            fs.appendFileSync('js_log.txt', 'auth resolve'+'\r\n');
 
             resolve({
                 cookie: httpResponse.headers['set-cookie'][0],
@@ -40,9 +43,10 @@ promise.then((res) => {
         request.get(access.host + '/apanel/' + res.location,
             (err, httpResponse, body) => {
                 if (err) {
+                    fs.appendFileSync('js_log.txt', 'dashboard reject: '+JSON.stringify(err)+'\r\n');
                     reject(err);
                 } else {
-                    fs.writeFile('pages/dashbordPromise.html', body);
+                    fs.appendFileSync('js_log.txt', 'dashboard resolve'+'\r\n');
                     resolve(body);
                 }
             }
@@ -57,9 +61,10 @@ promise.then((res) => {
         request.get(href,
             (err, httpResponse, body) => {
                 if (err) {
+                    fs.appendFileSync('js_log.txt', 'products list reject: '+JSON.stringify(err)+'\r\n');
                     reject(err);
                 } else {
-                    fs.writeFile('pages/productsPromise.html', body);
+                    fs.appendFileSync('js_log.txt', 'products list resolve'+'\r\n');
                     resolve(href);
                 }
             }
@@ -68,18 +73,17 @@ promise.then((res) => {
 })
 .then((res) => {
     return new Promise((resolve, reject) => {
-        let productsPromiseArray = [];
-
-        function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        }
-
         /*products.meubel*/
         let indexNum = 0;
 
         let array = products.meubel;
 
         let interval = setInterval(() => {
+            if (indexNum === array.length) {
+              clearInterval(interval);
+              return;
+            }
+
             let id = array[indexNum];
             console.log(id);
             indexNum++;
@@ -119,7 +123,9 @@ promise.then((res) => {
                                         var matches = [];
 
                                         body.replace(/var selected_categories = new Array\("(.*?)"\);/, function(a, val, match) {
-                                            matches.push(val);
+                                          val.replace(/"([^"]+(?="))"/g, '$1').split(',').forEach((categoriesItem) => {
+                                            matches.push(categoriesItem);
+                                          });
                                         });
 
                                         parsed.categoryBox = matches;
@@ -133,35 +139,31 @@ promise.then((res) => {
                 );
             });
 
-            productsPromiseArray.push(new Promise((productResolve, productReject) => {
-                Promise.all(promiseArray)
-                    .then((arg) => {
-                        parsed.selectedCarriers = ['2', '34', '1'];
-                        parsed.submitAddproduct = 1;
-                        parsed.submitted_tabs = ['Shipping'];
+            Promise.all(promiseArray)
+              .then((arg) => {
+                  parsed.selectedCarriers = ['2', '34', '1'];
+                  parsed.submitAddproduct = 1;
+                  parsed.submitted_tabs = ['Shipping'];
 
-                        fs.writeFile('pages/parsed.html', queryString.stringify(parsed));
+                  fs.appendFileSync('js_log.txt', 'product update request data'+ queryString.stringify(parsed)+'\r\n');
 
-                        request.post({
-                            url: href + '&key_tab=Shipping',
-                            form: parsed
-                        }, function (err, httpResponse, body) {
-                            console.log(body);
-                            fs.writeFile('pages/meubel'+id+'.html', body);
-                        });
-
-                        productResolve(parsed);
-                    }, (arg) => {
-                        console.log('productReject');
-                        productReject(parsed);
-                    });
-                })
-            );
-
-            if (indexNum === array.length - 1) {
-                resolve(indexNum);
-            }
-
+                  request.post({
+                      url: href + '&key_tab=Shipping',
+                      form: parsed
+                  }, function (err, httpResponse, body) {
+                      if (err) {
+                        fs.appendFileSync('js_log.txt', 'product update reject. ID'+id+'\r\n');
+                        fs.appendFileSync('js_log.txt', 'product update reject: '+JSON.stringify(err)+'\r\n');
+                        fs.appendFileSync('js_log.txt', '----------------\r\n\r\n');
+                      } else {
+                        fs.writeFile('pages/meubel'+id+'.html', body, () => {});
+                        fs.appendFileSync('js_log.txt', 'product update complete. ID: '+id+'\r\n');
+                        fs.appendFileSync('js_log.txt', '++++++++++++++++\r\n\r\n');
+                      }
+                  });
+              }, (arg) => {
+                  fs.appendFileSync('js_log.txt', 'product update reject: '+JSON.stringify(parsed)+'\r\n');
+              });
         }, 1000);
     });
 });
---
+++ pass_example.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: 'http://www.local-belgameubelen.be/',
+  path: '/apanel/index.php?controller=AdminLogin&token=',
+  token: 'a9810e5a4510cbca016d79c8d33e1672',
+  email: 'EMAIL',
+  pass: 'PASS'
+};
